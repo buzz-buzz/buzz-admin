7e5b1cfdb6904474f24e22ecdc828dbd92ccb3c2
Jefåf Tian <jeff.tian@outlook.com>
2018-02-06 14:14:37 +0800

    add basic auth

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const Koa = require('koa');
 const app = new Koa();
+const auth = require('koa-basic-auth');
 
-app.use(async ctx => {
-    ctx.body = 'Welcome to BuzzAdmin';
+app.use(async (ctx, next) => {
+    try {
+        await next();
+    } catch (err) {
+        if (401 == err.status) {
+            ctx.status = 401;
+            ctx.set('WWW-Authenticate', 'Basic');
+            ctx.body = 'You don\'t have privilege to access this.';
+        } else {
+            throw err;
+        }
+    }
 });
 
-app.listen(process.env.PORT || 16666);
+app.use(auth({name: 'buzzadmin', pass: 'cool'}));
+
+app.use(async ctx => {
+    ctx.body = 'Welcome to buzz admin';
+});
+
+app.listen(process.env.PORT || 16666, function () {
+    console.log('started!');
+});
