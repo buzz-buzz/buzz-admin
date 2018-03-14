02ae43f65860e011dbf47b031a4b969f22e61a0a
Jefåf Tian <jeff.tian@outlook.com>
2018-03-14 15:35:00 +0800

    Delete unused files and add version to cdn

--- views/cdn.js
+++ views/cdn.js
@@ -1,6 +1,6 @@
 module.exports = function (resource) {
     if (process.env.NODE_ENV === 'production') {
-        return `//cdn-admin-test.buzzbuzzenglish.com${resource}`
+        return `//cdn-admin-test.buzzbuzzenglish.com${resource}?v=2`
     }
 
     return resource;
