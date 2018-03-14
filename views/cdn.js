75dce3f7a093b73b8915ccb49dba12c2ec00dc0f
Jefåf Tian <jeff.tian@outlook.com>
2018-03-14 15:52:10 +0800

    bumpup

--- views/cdn.js
+++ views/cdn.js
@@ -1,6 +1,6 @@
 module.exports = function (resource) {
     if (process.env.NODE_ENV === 'production') {
-        return `//cdn-admin-test.buzzbuzzenglish.com${resource}?v=2`
+        return `//cdn-admin-test.buzzbuzzenglish.com${resource}?v=3`
     }
 
     return resource;
