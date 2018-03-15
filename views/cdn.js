bcfa027720625d861dc6fb9d6c6d4ec248a6d051
Jefåf Tian <jeff.tian@outlook.com>
2018-03-15 17:56:21 +0800

    Update cdn version

--- views/cdn.js
+++ views/cdn.js
@@ -1,6 +1,6 @@
 module.exports = function (resource) {
     if (process.env.NODE_ENV === 'production') {
-        return `//cdn-admin-test.buzzbuzzenglish.com${resource}?v=3`
+        return `//cdn-admin-test.buzzbuzzenglish.com${resource}?v=4`
     }
 
     return resource;
