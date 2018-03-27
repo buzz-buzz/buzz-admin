00e5aa83c493fe0d745c29bd1675f02539201bad
Jefåf Tian <jeff.tian@outlook.com>
2018-03-27 18:16:36 +0800

    Read version from package.json

--- views/cdn.js
+++ views/cdn.js
@@ -1,11 +1,14 @@
+let package = require('../package.json');
+
 module.exports = function (resource) {
+    let version = package.version.replace(/\./g, '-')
     if (process.env.NODE_ENV === 'production') {
-        return `//cdn-admin.buzzbuzzenglish.com${resource}?v=7`
+        return `//cdn-admin.buzzbuzzenglish.com${resource}?v=${version}`
     }
 
     if (process.env.NODE_ENV === 'qa') {
-        return `//cdn-admin-test.buzzbuzzenglish.com${resource}?v=7`
+        return `//cdn-admin-test.buzzbuzzenglish.com${resource}?v=${version}`
     }
 
-    return resource;
+    return `${resource}?v=${version}`;
 }
