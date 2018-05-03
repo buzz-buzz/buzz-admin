35af4417a522f9c4116749170c42494ccb4acf84
Jeff Tian <Jeff.Tian@outlook.com>
2018-05-03 16:05:13 +0800

    add flag

--- public/js/common/Countries.js
+++ public/js/common/Countries.js
@@ -4,6 +4,7 @@ export const list = Object.keys(countries).map(key => {
     return {
         key: key,
         text: countries[key].name,
-        value: countries[key].name
+        value: countries[key].name,
+        flag: key.toLowerCase()
     };
 });
