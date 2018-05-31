87b5df4f6824f76fc5ad404940a2667cbadf862e
hank <13061710755@sina.cn>
2018-05-31 16:03:25 +0800

    placement

--- public/js/components/students/level-modal.jsx
+++ public/js/components/students/level-modal.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Form, Header, Icon, Message, Modal, TextArea, Dropdown} from "semantic-ui-react";
+import { Form, Header, Icon, Message, Modal, TextArea, Dropdown } from "semantic-ui-react";
 import ServiceProxy from "../../service-proxy";
 
 export default class LevelModal extends React.Component {
@@ -18,7 +18,7 @@ export default class LevelModal extends React.Component {
         this.saveLevel = this.saveLevel.bind(this);
     }
 
-    handleChange(e, {name, value}) {
+    handleChange(e, { name, value }) {
         this.setState({
             [name]: value
         })
@@ -35,7 +35,7 @@ export default class LevelModal extends React.Component {
         })
 
         if (nextProps.user && nextProps.user.user_id && !this.state.detail) {
-            this.setState({loading: true});
+            this.setState({ loading: true });
 
             try {
                 let result = await ServiceProxy.proxyTo({
@@ -46,7 +46,7 @@ export default class LevelModal extends React.Component {
                 })
 
                 console.log('result = ', result);
-                this.setState({levelDetail: result.detail});
+                this.setState({ levelDetail: result.detail });
 
                 if (result.detail) {
                     this.setState({
@@ -59,16 +59,16 @@ export default class LevelModal extends React.Component {
                 }
 
             } catch (error) {
-                this.setState({error: true, message: JSON.stringify(error.result || error.message || error)});
+                this.setState({ error: true, message: JSON.stringify(error.result || error.message || error) });
             } finally {
-                this.setState({loading: false})
+                this.setState({ loading: false })
             }
         }
     }
 
     async saveLevel() {
         try {
-            this.setState({loading: true, error: false});
+            this.setState({ loading: true, error: false });
             let result = await ServiceProxy.proxyTo({
                 body: {
                     uri: `{buzzService}/api/v1/user-placement-tests/${this.state.userId}`,
@@ -91,7 +91,7 @@ export default class LevelModal extends React.Component {
                 message: JSON.stringify(error.result)
             })
         } finally {
-            this.setState({loading: false})
+            this.setState({ loading: false })
         }
     }
 
@@ -100,33 +100,33 @@ export default class LevelModal extends React.Component {
     }
 
     render() {
-        const {level} = this.state;
+        const { level } = this.state;
         return (
             <Modal open={this.props.open} closeOnEscape={true} closeOnRootNodeClick={false} onClose={this.close}>
                 <Header content="能力评级"></Header>
                 <Modal.Content>
                     <p>当前评级：{this.state.level}</p>
                     <Form error={this.state.error} loading={this.state.loading}>
-                        <Message error header="出错了" content={this.state.message}/>
+                        <Message error header="出错了" content={this.state.message} />
                         {
                             !this.state.jsonDetail.questions &&
 
                             <Form.Group>
                                 <TextArea autoHeight rows={3} value={this.state.levelDetail}
-                                          placeholder="答题详情"></TextArea>
+                                    placeholder="答题详情"></TextArea>
                             </Form.Group>
                         }
                         <Form.Group>
-                            <Dropdown selection name="update_level"  onChange={this.handleChange}
-                                                  options={[{key: '1', value: '1', text: '1'},
-                                                  {key: '2', value: '2', text: '2'},
-                                                  {key: '3', value: '3', text: '3'},
-                                                  {key: '4', value: '4', text: '4'},
-                                                  {key: '5', value: '5', text: '5'},
-                                                  {key: '6', value: '6', text: '6'},]}
-                                                  value={this.state.update_level} placeholder="能力评级"/>
-                            <Form.Button content="保存" type="button" onClick={this.saveLevel}/>
-                            <Form.Button className="right floated" content="取消" type="button" onClick={this.close}/>
+                            <Dropdown selection name="update_level" onChange={this.handleChange}
+                                options={[{ key: '1', value: '1', text: '1' },
+                                { key: '2', value: '2', text: '2' },
+                                { key: '3', value: '3', text: '3' },
+                                { key: '4', value: '4', text: '4' },
+                                { key: '5', value: '5', text: '5' },
+                                { key: '6', value: '6', text: '6' },]}
+                                value={this.state.update_level} placeholder="能力评级" />
+                            <Form.Button content="保存" type="button" onClick={this.saveLevel} />
+                            <Form.Button className="right floated" content="关闭" type="button" onClick={this.close} />
                         </Form.Group>
                     </Form>
                     <ol>
@@ -140,7 +140,7 @@ export default class LevelModal extends React.Component {
                                                 <li key={i}>
                                                     {
                                                         this.state.jsonDetail.answers[i].charCodeAt(0) + i - 'A'.charCodeAt(0) === 0 &&
-                                                        <Icon name="checkmark box"/>
+                                                        <Icon name="checkmark box" />
                                                     }
                                                     {item}
                                                 </li>
@@ -153,17 +153,17 @@ export default class LevelModal extends React.Component {
                         {
                             this.state.jsonDetail.answers &&
                             <li>
-                                <div style={{marginTop: '15px', display: 'flex', alignItems: 'center'}}>
+                                <div style={{ marginTop: '15px', display: 'flex', alignItems: 'center' }}>
                                     <span>录音题答案:</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                    <audio controls style={(typeof this.state.jsonDetail.answers[this.state.jsonDetail.questions.length] === 'object' ? this.state.jsonDetail.answers[this.state.jsonDetail.questions.length].url : this.state.jsonDetail.answers[this.state.jsonDetail.questions.length]).indexOf('m3u8') > -1 ? {display: 'none'} :{} }
+                                    <audio controls style={(typeof this.state.jsonDetail.answers[this.state.jsonDetail.questions.length] === 'object' ? this.state.jsonDetail.answers[this.state.jsonDetail.questions.length].url : this.state.jsonDetail.answers[this.state.jsonDetail.questions.length]).indexOf('m3u8') > -1 ? { display: 'none' } : {}}
                                         src={typeof this.state.jsonDetail.answers[this.state.jsonDetail.questions.length] === 'object' ? this.state.jsonDetail.answers[this.state.jsonDetail.questions.length].url : this.state.jsonDetail.answers[this.state.jsonDetail.questions.length]}>
                                     </audio>
                                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                                     <a href={typeof this.state.jsonDetail.answers[this.state.jsonDetail.questions.length] === 'object' ? this.state.jsonDetail.answers[this.state.jsonDetail.questions.length].url : this.state.jsonDetail.answers[this.state.jsonDetail.questions.length]}
-                                    target="_blank" rel="no-opener">原始音频文件</a>
+                                        target="_blank" rel="no-opener">原始音频文件</a>
                                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                                     <a href={`/m3u8/index.html?url=${encodeURIComponent(typeof this.state.jsonDetail.answers[this.state.jsonDetail.questions.length] === 'object' ? this.state.jsonDetail.answers[this.state.jsonDetail.questions.length].url : this.state.jsonDetail.answers[this.state.jsonDetail.questions.length])}`}
-                                    target="_blank" rel="no-opener">在线播放</a>
+                                        target="_blank" rel="no-opener">在线播放</a>
                                 </div>
                             </li>
                         }
