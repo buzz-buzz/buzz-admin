e253753e71d5f3200ff287f6313eeb664cc0989b
Jeff Tian <jeff.tian@buzzbuzzenglish.com>
2018-07-10 09:58:32 +0800

    show comment

--- public/js/components/feedbacks/feedback-card.jsx
+++ public/js/components/feedbacks/feedback-card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {connect} from 'react-redux';
-import {Card, Image} from "semantic-ui-react";
+import {Card, Image, Dimmer} from "semantic-ui-react";
 import store from '../../redux/store/index';
 import ServiceProxy from "../../service-proxy";
 import {loadFeedbacks} from "../../redux/actions";
@@ -19,25 +19,28 @@ class FeedbackCard extends React.Component {
 
     render() {
         const {fromUserId, toUserId, classId, feedbacks} = this.props;
-        console.log('key = ', `${classId}-${fromUserId}-${toUserId}`);
         const feedback = feedbacks[`${classId}-${fromUserId}-${toUserId}`];
-        console.log('feedback = ', feedback, feedbacks);
-        return <Card>
-            <Image floated="right" size="mini" src={`/avatar/${toUserId}`} as='a' href={`/users/${toUserId}`}
-                   target="_blank"/>
+        return <Dimmer.Dimmable as={Card} dimmed={!feedback}>
             <Card.Content>
-                <Card.Header></Card.Header>
+                <Image floated="right" size="mini" src={`/avatar/${toUserId}`} as='a' href={`/users/${toUserId}`}
+                       target="_blank"/>
+                <Card.Header>给 {feedback && feedback.to_name}</Card.Header>
                 <Card.Meta>
                     {feedback && feedback.feedback_time ? moment(feedback.feedback_time).format('llll') : null}
                 </Card.Meta>
-                <Card.Description></Card.Description>
+                <Card.Description>
+                    {feedback && feedback.comment}
+                </Card.Description>
             </Card.Content>
             <Card.Content extra>
                 <a href={`/users/${fromUserId}`} target="_blank">
+                    <span>来自</span>
                     <Image avatar src={`/avatar/${fromUserId}`}/>
+                    <span>{feedback && feedback.from_name}</span>
                 </a>
             </Card.Content>
-        </Card>
+            <Dimmer active={!feedback} inverted/>
+        </Dimmer.Dimmable>
     }
 }
 
