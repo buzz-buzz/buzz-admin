3f9fefadb7922b76cff355024441a87039c2a92c
Jeff Tian <jeff.tian@buzzbuzzenglish.com>
2018-08-16 16:58:21 +0800

    add life cycles file

---
+++ public/js/common/LifeCycles.js
@@ -0,0 +1,9 @@
+export const StudentLifeCycles = {
+    Potential: '潜在用户',
+    Leads: 'Leads',
+    Purchased: '买单用户',
+    WaitingForPlacementTest: '等待 Placement Test',
+    WaitingForFirstClass: '待排课',
+    Renewals: '续费',
+    Refunded: '退费'
+}
