31e1e66d6e4b9a22e378a088596bcc00536a4f3b
Jeff Tian <jeff.tian@buzzbuzzenglish.com>
2018-08-20 17:58:35 +0800

    Update state menu bar

--- public/js/common/LifeCycles.js
+++ public/js/common/LifeCycles.js
@@ -1,16 +1,19 @@
 export const StudentLifeCycles = {
     Potential: '潜在用户',
-    Leads: 'Leads',
+    Lead: 'Leads',
     Demo: 'Demo 用户',
-    Purchased: '买单用户',
-    WaitingForPlacementTest: '等待 Placement Test',
-    WaitingForFirstClass: '待排课',
-    Renewals: '续费',
-    Refunded: '退费'
+    Waiting_Purchase: '待买单',
+    In_Class: '正式上课',
+    Waiting_Renewal: '需续费',
+    Invalid: '无效用户'
 }
 
 export const StudentLifeCyclesMapping = {
     potential: StudentLifeCycles.Potential,
-    lead: StudentLifeCycles.Leads,
+    lead: StudentLifeCycles.Lead,
     demo: StudentLifeCycles.Demo,
+    waiting_purchase: StudentLifeCycles.Waiting_Purchase,
+    in_class: StudentLifeCycles.In_Class,
+    waiting_renewal: StudentLifeCycles.Waiting_Renewal,
+    invalid: StudentLifeCycles.Invalid
 }
