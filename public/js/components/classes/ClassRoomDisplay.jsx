9f5a7c6a7cbf47db03965fa91cf6181a719f0c1e
hank <13061710755@sina.cn>
2018-09-30 11:02:04 +0800

    roomUrl

--- public/js/components/classes/ClassRoomDisplay.jsx
+++ public/js/components/classes/ClassRoomDisplay.jsx
@@ -5,6 +5,6 @@ function getZoomRoomId(zoomUrl) {
 }
 
 export default ({roomUrl}) => <a onClick={e => e.stopPropagation()}
-                                 href={(roomUrl.startsWith('http://') || roomUrl.startsWith('https://') || roomUrl.startsWith('//')) ? roomUrl : 'javascript: void(0)'}
+                                 href={ roomUrl && (roomUrl.startsWith('http://') || roomUrl.startsWith('https://') || roomUrl.startsWith('//')) ? roomUrl : 'javascript: void(0)'}
                                  target="_blank"
                                  rel="noreferrer noopener">{getZoomRoomId(roomUrl)}</a>
