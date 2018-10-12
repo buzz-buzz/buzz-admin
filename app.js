f00f7c6fd35b17b33aee08c814623ae90b7bb5aa
hank <13061710755@sina.cn>
2018-10-12 14:50:25 +0800

    version

--- app.js
+++ app.js
@@ -108,6 +108,7 @@ router
     .get('/students/:userId?', clientPage)
     .get('/companions/:userId?', clientPage)
     .get('/classes', clientPage)
+    .get('/version', clientPage)
     .get('/feedbacks/:class_id', clientPage)
     .get('/avatar/:userId', async ctx => {
         let profile = await request(`${config.endPoints.buzzService}/api/v1/users/${ctx.params.userId}`, {
