e264f8856311d0df771489061c9aaef240037a9a
hank <13061710755@sina.cn>
2018-12-11 18:03:23 +0800

    forrmat integral time

--- public/js/components/common/time-display.jsx
+++ public/js/components/common/time-display.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 export default ({timestamp, format, timeFix}) => timestamp ? <div>
                                 <span
-                                    style={{whiteSpace: 'nowrap'}}>{!timeFix ? moment(timestamp).format(format || 'LL') : moment(timestamp).subtract(8, 'h') }</span><br/>
+                                    style={{whiteSpace: 'nowrap'}}>{!timeFix ? moment(timestamp).format(format || 'LL') : moment(timestamp).subtract(8, 'hours').format(format || 'LL') }</span><br/>
     <span
-        style={{color: 'lightgray'}}>{!timeFix ? moment(timestamp).calendar() : moment(timestamp).subtract(8, 'h').calendar() }</span>，
+        style={{color: 'lightgray'}}>{!timeFix ? moment(timestamp).calendar() : moment(timestamp).subtract(8, 'hours').calendar() }</span>，
     <span
-        style={{color: 'lightgray'}}>{!timeFix ? moment(timestamp).fromNow() : moment(timestamp).subtract(8, 'h').fromNow() }</span>
+        style={{color: 'lightgray'}}>{!timeFix ? moment(timestamp).fromNow() : moment(timestamp).subtract(8, 'hours').fromNow() }</span>
 </div> : null
